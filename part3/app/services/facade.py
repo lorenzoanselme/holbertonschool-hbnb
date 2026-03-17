9e23ae521415e3f7f14dd330ad1a0ae9632617e0
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-03-17 12:56:44 +0000

    Feat: add UserRepository and refactor facade to use dedicated user repository

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import SQLAlchemyRepository
+from app.services.repositories.user_repository import UserRepository
 from app.models.amenity import Amenity
 from app.models.user import User
 from app.models.place import Place
@@ -7,14 +8,20 @@ from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = SQLAlchemyRepository(User)
+        self.user_repo = UserRepository()
         self.place_repo = SQLAlchemyRepository(Place)
         self.review_repo = SQLAlchemyRepository(Review)
         self.amenity_repo = SQLAlchemyRepository(Amenity)
 
     # User
     def create_user(self, user_data):
-        user = User(**user_data)
+        user = User(
+            first_name=user_data['first_name'],
+            last_name=user_data['last_name'],
+            email=user_data['email'],
+            password=user_data['password'],
+            is_admin=user_data.get('is_admin', False)
+        )
         self.user_repo.add(user)
         return user
 
@@ -22,7 +29,7 @@ class HBnBFacade:
         return self.user_repo.get(user_id)
 
     def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute("email", email)
+        return self.user_repo.get_user_by_email(email)
 
     def get_all_users(self):
         return self.user_repo.get_all()
